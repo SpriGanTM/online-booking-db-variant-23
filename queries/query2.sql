49d72f3b2fae0e5bff2c829d61514915814db01d
SpriGanTM <Forgames5656you@gmail.com>
2026-05-19 13:18:04 +0700

    query2.sql

--- queries/query2.sql
+++ queries/query2.sql
@@ -1,1 +1,10 @@
-
+SELECT
+    m.last_name,
+    m.first_name,
+    COUNT(r.repair_id) AS total_repairs
+FROM masters m
+LEFT JOIN repairs r
+ON m.master_id = r.master_id
+GROUP BY m.master_id
+HAVING total_repairs > 1
+ORDER BY total_repairs DESC;
