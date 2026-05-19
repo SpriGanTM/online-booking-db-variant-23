c2838d7b995628da1c52c3ecda333b2a7fbd1f1e
SpriGanTM <Forgames5656you@gmail.com>
2026-05-19 13:18:20 +0700

    query3.sql

--- queries/query3.sql
+++ queries/query3.sql
@@ -1,1 +1,22 @@
+SELECT
+    m.last_name,
+    m.first_name,
 
+    AVG(
+        TIMESTAMPDIFF(
+            MINUTE,
+            r.repair_datetime,
+            r.completion_datetime
+        )
+    ) AS avg_repair_time_minutes
+
+FROM masters m
+
+JOIN repairs r
+ON m.master_id = r.master_id
+
+WHERE r.status = 'завершён'
+
+GROUP BY m.master_id
+
+ORDER BY avg_repair_time_minutes;
